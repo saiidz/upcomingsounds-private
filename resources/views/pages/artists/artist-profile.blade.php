79c18aa8cc438985207bfc8cb85935831af95066
farhan akram <farhanakram670@gmail.com>
2021-11-13 23:24:34 +0500

    Artist work

--- resources/views/pages/artists/artist-profile.blade.php
+++ resources/views/pages/artists/artist-profile.blade.php
@@ -97,7 +97,7 @@
                                         {{$user_artist->artistUser->artist_bio}}
                                     @endif
                                 </p>
-                                @if(!empty($user_artist->artistUser->country))
+{{--                                @if(!empty($user_artist->artistUser->country))--}}
                                     <div class="block flag_style clearfix m-b">
 {{--                                        @foreach($countries_flag as $flag)--}}
 {{--                                            @if($flag['iso_a2'] == $user_artist->artistUser->country->iso2)--}}
@@ -107,7 +107,7 @@
                                         <span class="text-muted"
                                               style="font-size:15px">{{($user_artist->artistUser->country) ? $user_artist->artistUser->country->name : ''}}</span>
                                     </div>
-                                @endif
+{{--                                @endif--}}
                                 <form class="profile-pic" id="profileBtnShow" method="post"
                                       enctype="multipart/form-data" style="display:none;">
                                     <div class="item-action m-b">
