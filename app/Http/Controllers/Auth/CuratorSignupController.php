1fbfbc2eeb4449e19adb6fb5eed3131f86ed2bb4
farhan akram <farhanakram670@gmail.com>
2021-12-21 23:29:57 +0500

    fix

--- app/Http/Controllers/Auth/CuratorSignupController.php
+++ app/Http/Controllers/Auth/CuratorSignupController.php
@@ -533,6 +533,7 @@ class CuratorSignupController extends Controller
                 return response()->json(['error' => 'You have entered instagram invalid url. Please add correct url']);
             }
             $response = Http::get("https://www.instagram.com/$username/?__a=1");
+            dd($response);
             if($response->status() == 404){
                 return response()->json(['error' => 'You have entered instagram invalid url. Please add correct url']);
             }
