5ea30e085be21d9ecb00d422dc3178bddb876776
farhan akram <farhanakram670@gmail.com>
2021-12-24 01:30:58 +0500

    fix

--- app/Http/Controllers/Auth/CuratorSignupController.php
+++ app/Http/Controllers/Auth/CuratorSignupController.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
 use Sovit\TikTok;
-//use MetzWeb\Instagram\Instagram;
 
 class CuratorSignupController extends Controller
 {
@@ -533,14 +532,8 @@ class CuratorSignupController extends Controller
             if(empty($username)){
                 return response()->json(['error' => 'You have entered instagram invalid url. Please add correct url']);
             }
-//            $instagram = new Instagram(array(
-//                'apiKey'      => '403018831604030',
-//                'apiSecret'   => 'e537c2cc0d93af5d8a262b6501200fa8',
-//                'apiCallback' => 'YOUR_APP_CALLBACK'
-//            ));
-            $html = file_get_contents('http://www.instagram.com/'.$username.'/?__a=1');
-dd($html);
-            $response = Http::get("https://www.instagram.com/$username/?__a=1");
+
+            $response = Http::dd()->get("https://www.instagram.com/$username/?__a=1");
 
             if($response->status() == 404){
                 return response()->json(['error' => 'You have entered instagram invalid url. Please add correct url']);
