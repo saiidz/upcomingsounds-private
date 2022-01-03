b5da0e001c06bb8c8f76136483ba2aa6ac42ca3e
farhan akram <farhanakram670@gmail.com>
2022-01-04 01:43:50 +0500

    fix video loop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -207,3 +207,12 @@
             @include('welcome-panels.welcome-footer')
 
 @endsection
+{{-- page script --}}
+@section('page-script')
+    <script>
+        $('video').on('ended', function () {
+            this.load();
+            this.play();
+        });
+    </script>
+@endsection
