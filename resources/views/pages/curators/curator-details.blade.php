6fe8a72d3f497593c9444a92d3bf1619e977b37c
saiidz <saiidzeidan@gmail.com>
2022-01-12 15:04:43 +0200

    Update curator-details.blade.php

--- resources/views/pages/curators/curator-details.blade.php
+++ resources/views/pages/curators/curator-details.blade.php
@@ -87,10 +87,10 @@
                                 <path d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"></path>
                             </svg>
                             <h5>
-                                <span>Earn money</span>
+                                <span>REDEEM REWARDED</span>
                             </h5>
                             <p class="text-muted text-md">
-                                <span>When an artist sends you a "premium" submission they're  asking for your feedback about that song. If you respond in time, you'll earn $0.50. Many curators re-invest the money they earn for marketing - but we leave that decision to you. Payments are made via PayPal or directly to your bank account.</span>
+                                <span>Artists when submitting their music, they're asking for your feedback about their song. If you respond on time, you will receive a standard Fee of 1 USC coin = 1 GBP standard fee for "Non-verified" Tastemakers / Curators / Professionals. You can always submit your platform to our editors to increase your submissions Fee. Payments are made via PayPal or directly to your bank account.</span>
                             </p>
                         </div>
                         <div class="clearfix"></div>
