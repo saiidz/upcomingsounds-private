22989f2ac4dc31c46471d7ef8b0cb13748408311
farhan akram <farhanakram670@gmail.com>
2022-01-14 01:54:15 +0500

    add svg icon

--- resources/views/pages/artists/artist-details.blade.php
+++ resources/views/pages/artists/artist-details.blade.php
@@ -49,9 +49,10 @@
 
                     <div class="container landing-bullets">
                         <div class="col-xs-12 col-sm-12 m5 bullets">
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"></path>
-                            </svg>
+                            <img src="{{asset('images/verified_pros_curators.svg')}}">
+{{--                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">--}}
+{{--                                <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"></path>--}}
+{{--                            </svg>--}}
                             <h5>
                                 <span>Verified Pros / Curators </span>
                             </h5>
