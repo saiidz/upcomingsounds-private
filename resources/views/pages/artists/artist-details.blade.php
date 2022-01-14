1175f18809548bd6de77ab3164832d41f55c0a1a
saiidz <saiidzeidan@gmail.com>
2022-01-14 18:04:50 +0200

    Update artist-details.blade.php

--- resources/views/pages/artists/artist-details.blade.php
+++ resources/views/pages/artists/artist-details.blade.php
@@ -44,7 +44,7 @@
                         <h5>
                             <span>1- Signup   >>>   2-uploud your song  >>>    3- select curators and send your song. </span>
                         </h5>
-                         <div>
+                       
                             <span>
                                 <p class="text-muted text-md">UpcomingSounds allows you to easily send your music to curators using USC Coins' "credits." Each curator requests the standard fee of 2 USC coins, Verified curators, tastemakers, and pors require 4 to  50 credits depending on their level.</p>
                                 <p class="text-muted text-md">It doesn't matter if you are just starting and sending demos or if your work is already established and ready to be shown, we are here to help. you can expect your submission decision within 96 hours of receiving it. We will connect you with curators, blogs, journalists and other professionals that can get you the exposure you need in the industry. We can guarantee that you will get feedback from every track you submit. Sometimes the feedback you receive when your song is declined will be useful.</p>
