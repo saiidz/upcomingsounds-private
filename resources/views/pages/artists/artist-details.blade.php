ab0e1b2b28135c3550aff2978e03fb65cdd3fde2
farhan akram <farhanakram670@gmail.com>
2022-01-15 14:28:29 +0500

    fix artist detail

--- resources/views/pages/artists/artist-details.blade.php
+++ resources/views/pages/artists/artist-details.blade.php
@@ -50,6 +50,32 @@
                                 <p class="text-muted text-md">It doesn't matter if you are just starting and sending demos or if your work is already established and ready to be shown, we are here to help. you can expect your submission decision within 96 hours of receiving it. We will connect you with curators, blogs, journalists and other professionals that can get you the exposure you need in the industry. We can guarantee that you will get feedback from every track you submit. Sometimes the feedback you receive when your song is declined will be useful.</p>
                             </span>
                         </div>
+{{--                        @if(Auth::check() && Auth::user()->type == 'artist')--}}
+{{--                            <a href="{{url('/artist-profile')}}" class="transparent  tellMeMore">--}}
+{{--                                <span>Dashboard</span>--}}
+{{--                            </a>--}}
+{{--                        @else--}}
+{{--                            <a href="{{url('/login')}}" class="transparent  tellMeMore">--}}
+{{--                                <span>Start Now</span>--}}
+{{--                            </a>--}}
+
+{{--                        @endif--}}
+{{--                        <div class="clearfix"></div>--}}
+                    </div>
+
+                    <div class="container landing-headline-works">
+                        <h3>
+                            <span>How it Works</span>
+                        </h3>
+                        <div class="mrg-bottom-30">
+                            <h5>
+                                <span>1- Signup   >>>   2-uploud your song  >>>    3- select curators and send your song. </span>
+                            </h5>
+                            <span>
+                                <p class="text-muted text-md">UpcomingSounds allows you to easily send your music to curators using USC Coins' "credits." Each curator requests the standard fee of 2 USC coins, Verified curators, tastemakers, and pors require 4 to  50 credits depending on their level.</p>
+                                <p class="text-muted text-md">It doesn't matter if you are just starting and sending demos or if your work is already established and ready to be shown, we are here to help. you can expect your submission decision within 96 hours of receiving it. We will connect you with curators, blogs, journalists and other professionals that can get you the exposure you need in the industry. We can guarantee that you will get feedback from every track you submit. Sometimes the feedback you receive when your song is declined will be useful.</p>
+                            </span>
+                        </div>
                         @if(Auth::check() && Auth::user()->type == 'artist')
                             <a href="{{url('/artist-profile')}}" class="transparent  tellMeMore">
                                 <span>Dashboard</span>
