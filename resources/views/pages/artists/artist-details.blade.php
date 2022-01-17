06cc5994a8af5dc542d89972edf25ba5face818c
saiidz <saiidzeidan@gmail.com>
2022-01-17 20:36:42 +0200

    Update artist-details.blade.php

--- resources/views/pages/artists/artist-details.blade.php
+++ resources/views/pages/artists/artist-details.blade.php
@@ -129,7 +129,7 @@
                                 <span>Direct Contact</span>
                             </h5>
                             <p class="text-muted text-md">
-                                <span>No emailing is required. Our Dashboard grants you direct communication with artists. You'll be able to manage everything from UpcomingSounds.</span>
+                                <span>No emailing is required. Our Dashboard grants you direct communication with curators and professionals. You'll be able to send them your songs and read their reviews on your UpcomingSounds Dashboard.</span>
                             </p>
                         </div>
                         <div class="col-xs-12 col-sm-12 m5 bullets offset-m1">
