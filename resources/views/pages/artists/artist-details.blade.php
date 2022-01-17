92f9fa18bdbf3abe6aa0d5f00747c74d14e9bddc
saiidz <saiidzeidan@gmail.com>
2022-01-17 19:14:19 +0200

    Update artist-details.blade.php

--- resources/views/pages/artists/artist-details.blade.php
+++ resources/views/pages/artists/artist-details.blade.php
@@ -149,10 +149,10 @@
                                 <path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 5h-3v5.5c0 1.38-1.12 2.5-2.5 2.5S10 13.88 10 12.5s1.12-2.5 2.5-2.5c.57 0 1.08.19 1.5.51V5h4v2zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6z"></path>
                             </svg>
                             <h5>
-                                <span>For labels</span>
+                                <span>For Artist reprasentitve/ Managers / Lebales</span>
                             </h5>
                             <p class="text-muted text-md">
-                                <span>Discover new talent and open conversations with prospective artists. You can also re-invest any credits you earn to submit your roster's music to blogs and playlisters.</span>
+                                <span>We have created a multi-artist managing profile to help you easily access the dashboard for multiple artists with no extra fees.</span>
                             </p>
                         </div>
                         <div class="clearfix"></div>
