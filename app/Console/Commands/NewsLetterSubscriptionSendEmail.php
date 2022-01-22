d4e544f6fc4d5d55364872509af7e4768479073f
farhan akram <farhanakram670@gmail.com>
2022-01-22 20:37:08 +0500

    done newsletter cron jobs

---
+++ app/Console/Commands/NewsLetterSubscriptionSendEmail.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\NewsLetterSubscription;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class NewsLetterSubscriptionSendEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'newsletter:subscription';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try{
+            $newsletters = NewsLetterSubscription::where('status',1)->get();
+            if(isset($newsletters)){
+                foreach ($newsletters as $newsletter){
+                    Log::info('Processed NewsletterID: '.$newsletter->id);
+                    $email = new \SendGrid\Mail\Mail();
+                    $email->setFrom( "info@upcomingsounds.com", "News Letter Upcoming Sounds" );
+                    $email->addTo($newsletter->email, "News Letter" );
+                    $email->setTemplateId(
+                        new \SendGrid\Mail\TemplateId( 'd-37e1d0fbc45d46e6bd30cb380e959f70' )
+                    );
+
+                    // === Here comes the dynamic template data! ===
+                    $email->addDynamicTemplateDatas( [
+                        'variable1'     => 'Some stuff',
+                        'templatesRock' => 'They sure do!'
+                    ] );
+
+                    $sendgrid = new \SendGrid( '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' );
+
+                    $response = $sendgrid->send( $email );
+                    Log::info('Processed Response: '.$response->statusCode());
+                }
+            }
+        }catch (\Exception $exception){
+            $this->info('Some error occurred while News Letter Subscription: '.$exception->getMessage());
+        }
+    }
+}
