ad97d084638c7f88d8b1caa14b06ca07a5d77cd7
farhan akram <farhanakram670@gmail.com>
2022-01-27 00:08:50 +0500

    add monthly cron job send email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('newsletter:subscription')->everyMinute();
+        $schedule->command('newsletter:subscription')->monthly();
     }
 
     /**
