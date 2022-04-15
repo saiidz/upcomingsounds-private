849f5233b7c5b5f82bdd0ed046e2d8992a7dfd8f
saiidz <saiidzeidan@gmail.com>
2022-04-15 16:09:45 +0300

    Update radio-details.blade.php

--- resources/views/pages/curators/curator-details-signup/radio-details.blade.php
+++ resources/views/pages/curators/curator-details-signup/radio-details.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.curator-guest')
 
 {{-- page title --}}
-@section('title','Taste Maker Signup ')
+@section('title','Radio / TV Signup ')
 
 {{-- page style --}}
 @section('page-style')
@@ -51,8 +51,8 @@
                                                     <h2 class="card-title bold" style="font-size: 20px;">Guaranteed shares</h2>
                                                     <ul class="instagram_details">
                                                         <li class="text-muted">You are required to listen to each submission that you receive at least 30 seconds to get paid and provide feedback within 96 hours.</li>
-                                                        <li class="text-muted">If you decide not to share their song, you will get paid (you must provide a reason why you did not share the song).</li>
-                                                        <li class="text-muted">If you are not a verified  Playlist Curator, you will receive the standard fee of 1 USC coin = 1 GBP</li>
+                                                        <li class="text-muted">If you decide not to share their song or write feedback on your Radio / TV, you must provide a rejection reason to get paid. If you do not provide a rejection reason, the artists will get a refund..</li>
+                                                        <li class="text-muted">Unless you are a verified Blog, you will receive the standard fee of 1 USC coin = 1 GBP per submission</li>
                                                     </ul>
                                                 </div>
                                             </div>
@@ -61,8 +61,8 @@
                                                     <h2 class="card-title bold" style="font-size: 20px;">Sharing songs</h2>
                                                     <ul class="instagram_details">
                                                         <li class="text-muted">Share the song on your platform, add their song to your website or Spotify, Appple Music,  Deezer Playlist(s) etc...  for at least five days in a raw (top spots recommended).</li>
-                                                        <li class="text-muted">If your placements are high quality with impactful results, artists will want to send you more songs to promote & you will make more money.</li>
-                                                        <li class="text-muted">If someone complains we might have to refund them, so make it a good share!</li>
+                                                        <li class="text-muted">Artists will want to send you more songs to promote if your placements are high quality and have impactful results, so you will make more money.</li>
+                                                        <li class="text-muted">We might have to refund someone if they complain, so make it a good one!</li>
 
                                                     </ul>
                                                 </div>
