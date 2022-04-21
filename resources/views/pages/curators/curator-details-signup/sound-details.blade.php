9d070b451e7d399fd619ba2008c9abedd3682716
saiidz <saiidzeidan@gmail.com>
2022-04-21 13:14:12 +0300

    Update sound-details.blade.php

--- resources/views/pages/curators/curator-details-signup/sound-details.blade.php
+++ resources/views/pages/curators/curator-details-signup/sound-details.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.curator-guest')
 
 {{-- page title --}}
-@section('title','Monitor / Publisher / Sync Signup ')
+@section('title',' Sound Expert  / Engineer Signup   ')
 
 {{-- page style --}}
 @section('page-style')
