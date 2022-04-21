ddc2f9cfc1ec6190fbe81de7add31c1bdb74a7f7
saiidz <saiidzeidan@gmail.com>
2022-04-21 13:12:29 +0300

    Update playlist-deezer-details.blade.php

--- resources/views/pages/curators/curator-playlist-signup/playlist-deezer-details.blade.php
+++ resources/views/pages/curators/curator-playlist-signup/playlist-deezer-details.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.curator-guest')
 
 {{-- page title --}}
-@section('title','Taste Maker Signup ')
+@section('title','Deezer Playlist Curator Signup ')
 
 {{-- page style --}}
 @section('page-style')
