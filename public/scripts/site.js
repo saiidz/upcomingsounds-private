4e27a8ec1f4c973856d5a7c01267fe71496de981
farhanxint <107535527+farhanxint@users.noreply.github.com>
2022-07-15 12:30:11 +0500

    fix issue

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -5,7 +5,8 @@
     $(document).on('click.site', '.btn-more', function(e) {
       var $dp = $(this).next('.dropdown-menu');
       if( $dp.html() == "" ){
-        $dp.append('<a class="dropdown-item" href="#"><i class="fa fa-plus fa-fw text-left"></i> Add to Queque</a><a class="dropdown-item" href="#"><i class="fa fa-music fa-fw text-left"></i> Add to Playlist</a><div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fa fa-share-alt fa-fw text-left"></i> Share</a>');
+        $dp.append('<a class="dropdown-item" href="#"><i class="fa fa-plus fa-fw text-left"></i> Add to Queque</a><a class="dropdown-item" href="#"><i class="fa fa-music fa-fw text-left"></i> Add to Playlist</a>');
+        // $dp.append('<a class="dropdown-item" href="#"><i class="fa fa-plus fa-fw text-left"></i> Add to Queque</a><a class="dropdown-item" href="#"><i class="fa fa-music fa-fw text-left"></i> Add to Playlist</a><div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fa fa-share-alt fa-fw text-left"></i> Share</a>');
       }
 
       if( (e.pageY + $dp.height() + 60) > $('body').height() ){
@@ -17,7 +18,7 @@
       }
 
     });
-    
+
     $(document).on('click.site', '#search-result a', function(e) {
       $(this).closest('.modal').modal('hide');
     });
