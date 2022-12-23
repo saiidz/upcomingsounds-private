6a6ff160d2008e7c707faef6db11f298a060e4b8
farhanxint <107535527+farhanxint@users.noreply.github.com>
2022-12-23 18:46:01 +0500

    ok

--- resources/views/pages/artists/artist-public-profile/public-profile.blade.php
+++ resources/views/pages/artists/artist-public-profile/public-profile.blade.php
@@ -5,6 +5,8 @@
 
 @section('page-style')
     <link rel="stylesheet" href="{{asset('css/custom/custom.css')}}" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('css/gijgo.min.css')}}" type="text/css" />
 {{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />--}}
     <style>
         svg.svg-inline--fa {
@@ -67,6 +69,13 @@
                                 {{$user->artistUser->artist_bio}}
                             @endif
                         </p>
+                        @if(!empty($user->artistUser->country))
+                            <div class="block flag_style clearfix m-b">
+                                <img class="flag_icon" src="{{asset('images/flags')}}/{{$user->artistUser->country->flag_icon}}.png" alt="{{$user->artistUser->country->flag_icon}}">
+                                <span class="text-muted"
+                                      style="font-size:15px">{{($user->artistUser->country) ? $user->artistUser->country->name : ''}}</span>
+                            </div>
+                        @endif
                         <div class="item-action m-b">
 {{--                            <a class="btn btn-icon white rounded btn-share pull-right" href="javascript:void(0)" id="shareArtistProfile" data-url_root="{{route('artist.public.profile',$user->name)}}" data-toggle="modal" data-target="#share-modal">--}}
 {{--                                <i class="fa fa-share-alt"></i>--}}
@@ -79,6 +88,83 @@
                                         {{$user->artistTrack->count()}}
                                     @endif Tracks</span>
                         </div>
+
+                        <div class="row-col m-b">
+                            <div class="col-xs">
+                                @if(!empty($user->artistUser->instagram_url))
+                                    <a href="{{$user->artistUser->instagram_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored light-blue-800"
+                                       title="Instagram">
+                                        <i class="fa fa-instagram"></i>
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->facebook_url))
+                                    <a href="{{$user->artistUser->facebook_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored indigo"
+                                       title="Facebook">
+                                        <i class="fa fa-facebook"></i>
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->spotify_url))
+                                    <a href="{{$user->artistUser->spotify_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored light-green-500"
+                                       title="Spotify">
+                                        <i class="fa fa-spotify"></i>
+                                        <i class="fa fa-spotify"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->soundcloud_url))
+                                    <a href="{{$user->artistUser->soundcloud_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored orange-700"
+                                       title="SoundCloud">
+                                        <i class="fa fa-soundcloud"></i>
+                                        <i class="fa fa-soundcloud"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->youtube_url))
+                                    <a href="{{$user->artistUser->youtube_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored red-600"
+                                       title="Youtube">
+                                        <i class="fa fa-youtube"></i>
+                                        <i class="fa fa-youtube"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->website_url))
+                                    <a href="{{$user->artistUser->website_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored"
+                                       style="background-color:#333;" title="Website">
+                                        <i class="fa fa-link"></i>
+                                        <i class="fa fa-link"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->deezer_url))
+                                    <a href="{{$user->artistUser->deezer_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored"
+                                       style="background-color:#eb9d00;" title="Deezer">
+                                        {{--                                                <span class="iconify" data-icon="fa-brands:deezer"></span>--}}
+                                        <i class="iconify" data-icon="fa-brands:deezer"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->bandcamp_url))
+                                    <a href="{{$user->artistUser->bandcamp_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored"
+                                       style="background-color:#333;" title="Bandcamp">
+                                        <i class="fa fa-bandcamp"></i>
+                                        <i class="fa fa-bandcamp"></i>
+                                    </a>
+                                @endif
+                                @if(!empty($user->artistUser->tiktok_url))
+                                    <a href="{{$user->artistUser->tiktok_url}}" target="_blank"
+                                       class="btn btn-icon btn-social rounded btn-social-colored"
+                                       style="background-color:#333;" title="Tiktok">
+                                        <i class="iconify" data-icon="fa-brands:tiktok"></i>
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -122,5 +208,6 @@
            $('#publicArtistLink').val(url);
         });
     </script>
+    <script src="{{asset('js/gijgo.min.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" async integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @endsection
