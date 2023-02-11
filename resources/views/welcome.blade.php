45b01a58fab5a6f57af1593a5fd82a22d21c7ea1
saiidz <saiidzeidan@gmail.com>
2023-02-11 23:31:37 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.welcomeLayout')
 
 {{-- page title --}}
-@section('title','Welcome to the newest platform for music promotion and pitching service with effective results. Submit your music now!')
+@section('title','Welcome to the newest platform for new and upcoming sounds, pitching service with effective results. Submit your music now!')
 @section('page-style')
     <style>
         .videoWlcome{
