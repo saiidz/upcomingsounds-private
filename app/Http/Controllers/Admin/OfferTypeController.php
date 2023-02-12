5c38128d424fb0a3a0bd8b5031cd43474ca59617
farhanxint <107535527+farhanxint@users.noreply.github.com>
2023-02-12 12:43:00 +0500

    ok

--- app/Http/Controllers/Admin/OfferTypeController.php
+++ app/Http/Controllers/Admin/OfferTypeController.php
@@ -136,7 +136,9 @@ class OfferTypeController extends Controller
      */
     public function offers()
     {
-        $curatorsOfferTemplates = User::with('curatorOfferTemplate')->GetApprovedCurators()->latest()->get();
+        $curatorsOfferTemplates = User::with('curatorOfferTemplate')->whereHas('curatorOfferTemplate', function ($q){
+            $q->where('type', '!=',IOfferTemplateStatus::TYPE_DIRECT_OFFER);
+        })->GetApprovedCurators()->latest()->get();
         dd($curatorsOfferTemplates);
         return view('admin.pages.offer-template.curator-offer-template', get_defined_vars());
 //        return view('admin.pages.offer-template.index', get_defined_vars());
