9566b4820be4a2f923f5aab534f7331dc536f338
farhanxint <107535527+farhanxint@users.noreply.github.com>
2023-03-23 01:23:47 +0500

    ok

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -215,7 +215,6 @@ class ArtistController extends Controller
     // forArtists
     public function forArtists()
     {
-        dump('aaa');
         $classifiedImg = public_path('images/artist-header.jpg');
         $image = Image::make($classifiedImg)->encode('webp', 90)->save(public_path('images/'  .  'artist-header.webp'));
 dd($image);
