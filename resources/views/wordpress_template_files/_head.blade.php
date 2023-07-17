90af610240ca6e41006313f6fe14f0ee87fef4a9
farhanxint <107535527+farhanxint@users.noreply.github.com>
2023-07-17 18:22:36 +0500

    ok

--- resources/views/wordpress_template_files/_head.blade.php
+++ resources/views/wordpress_template_files/_head.blade.php
@@ -1,5 +1,19 @@
+<!-- style -->
+<link rel="stylesheet" href="{{asset('css/animate.css/animate.min.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{asset('css/glyphicons/glyphicons.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{asset('css/font-awesome/css/font-awesome.min.css')}}" type="text/css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{asset('css/material-design-icons/material-design-icons.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{asset('css/bootstrap/dist/css/bootstrap.min.css')}}" type="text/css" />
+
 <!-- build:css css/styles/app.min.css -->
 <link rel="stylesheet" href="{{asset('css/styles/app.css')}}" type="text/css" />
 <link rel="stylesheet" href="{{asset('css/styles/style.css')}}" type="text/css" />
 <link rel="stylesheet" href="{{asset('css/styles/font.css')}}" type="text/css" />
 <link rel="stylesheet" href="{{asset('css/custom/custom.css')}}" type="text/css" />
+
+<link rel="stylesheet" href="{{asset('libs/owl.carousel/dist/assets/owl.carousel.min.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{asset('libs/owl.carousel/dist/assets/owl.theme.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{asset('libs/mediaelement/build/mediaelementplayer.min.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{asset('libs/mediaelement/build/mep.css')}}" type="text/css" />
