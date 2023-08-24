bfc801fcd14672c6dc3fb97a820442270e1c728d
Farhan Akram <34884414+farhanakram1@users.noreply.github.com>
2023-08-24 22:37:03 +0500

    gift card

--- resources/views/gift-card/index.blade.php
+++ resources/views/gift-card/index.blade.php
@@ -5,13 +5,16 @@
 @section('title','Gift Card')
 
 @section('page-style')
-    <link rel="stylesheet" type="text/css" href="{{asset('css/custom/faqs.css')}}">
     <style>
         .bgGift{
             background-image: url({{asset('images/gift_card_banner.png')}});
             background-size: 100%;
             background-position: top center;
             background-repeat: no-repeat;
+        }
+        .card-body{
+            text-align: center;
+            padding: 20px 20px;
         }
     </style>
 @endsection
@@ -24,14 +27,80 @@
 
         <div class="row-col bgGift h-v">
             <div class="row-cell v-m">
-                <div class="text-center col-sm-6 offset-sm-3 p-y-lg">
-                    <h1 class="display-3 m-y-lg">Gift Card</h1>
-                </div>
+{{--                <div class="text-center col-sm-6 offset-sm-3 p-y-lg">--}}
+{{--                    <h1 class="display-3 m-y-lg">Gift Card</h1>--}}
+{{--                </div>--}}
             </div>
         </div>
 
         <section class="cd-faq js-cd-faq container max-width-md margin-top-lg margin-bottom-lg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xs-12">
+                        <div class="card mb-6">
+                            <img src="{{ asset('images/gift-card/GC-USC-20.png')  }}" width="100%" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <a href="javascript:void(0)" class="nav-link">
+                                    <span class="btn btn-small-white rounded primary _600">{{ __('Buy Now') }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xs-12">
+                        <div class="card mb-6">
+                            <img src="{{ asset('images/gift-card/GC-USC-50.png')  }}" width="100%" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <a href="javascript:void(0)" class="nav-link">
+                                    <span class="btn btn-small-white rounded primary _600">{{ __('Buy Now') }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="cd-faq js-cd-faq container max-width-md margin-top-lg margin-bottom-lg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xs-12">
+                        <div class="card mb-6">
+                            <img src="{{ asset('images/gift-card/GC-USC-100.png')  }}" width="100%" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <a href="javascript:void(0)" class="nav-link">
+                                    <span class="btn btn-small-white rounded primary _600">{{ __('Buy Now') }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xs-12">
+                        <div class="card mb-6">
+                            <img src="{{ asset('images/gift-card/GC-USC-250.png')  }}" width="100%" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <a href="javascript:void(0)" class="nav-link">
+                                    <span class="btn btn-small-white rounded primary _600">{{ __('Buy Now') }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
+        <section class="cd-faq js-cd-faq container max-width-md margin-top-lg margin-bottom-lg" style="margin-bottom: 25px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xs-12">
+                        <div class="card mb-6">
+                            <img src="{{ asset('images/gift-card/GC-USC-500.png')  }}" width="100%" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <a href="javascript:void(0)" class="nav-link">
+                                    <span class="btn btn-small-white rounded primary _600">{{ __('Buy Now') }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
         <!-- ############ PAGE END-->
 
@@ -40,7 +109,5 @@
 @endsection
 
 @section('page-script')
-    <script>document.getElementsByTagName("html")[0].className += " js";</script>
-    <script src="{{asset('js/faq/util.js')}}"></script>
-    <script src="{{asset('js/faq/main.js')}}"></script>
+
 @endsection
