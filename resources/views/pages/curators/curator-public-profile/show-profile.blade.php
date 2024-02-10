d40cb428d439c6fd7c2f3e931de36f3f9e537360
Farhan Akram <34884414+farhanakram1@users.noreply.github.com>
2024-02-10 21:07:16 +0500

    changes

--- resources/views/pages/curators/curator-public-profile/show-profile.blade.php
+++ resources/views/pages/curators/curator-public-profile/show-profile.blade.php
@@ -17,25 +17,25 @@
 {{--</div>--}}
 @if(!empty($user->curatorUser))
     <div class="page-title m-b">
-        <h4 class="inline m-a-0 update_profile">TasteMaker Info</h4>
+        <h4 class="inline m-a-0 update_profile">Info</h4>
     </div>
 
-    <div class="form-group row">
-        <div class="col-sm-2 form-control-label">TasteMaker Signup From:</div>
-        <div class="col-sm-9">
-            <div
-                class="col-sm-3 form-control-label text-muted">{{($user->curatorUser) ? Str::upper($user->curatorUser->curator_signup_from) : ''}}</div>
-        </div>
-    </div>
+{{--    <div class="form-group row">--}}
+{{--        <div class="col-sm-2 form-control-label">TasteMaker Signup From:</div>--}}
+{{--        <div class="col-sm-9">--}}
+{{--            <div--}}
+{{--                class="col-sm-3 form-control-label text-muted">{{($user->curatorUser) ? Str::upper($user->curatorUser->curator_signup_from) : ''}}</div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
-    <div class="form-group row">
-        <div class="col-sm-2 form-control-label">TasteMaker Name:
-        </div>
-        <div class="col-sm-9">
-            <div
-                class="col-sm-3 form-control-label text-muted">{{isset($user->curatorUser) ? $user->curatorUser->tastemaker_name : ''}}</div>
-        </div>
-    </div>
+{{--    <div class="form-group row">--}}
+{{--        <div class="col-sm-2 form-control-label">TasteMaker Name:--}}
+{{--        </div>--}}
+{{--        <div class="col-sm-9">--}}
+{{--            <div--}}
+{{--                class="col-sm-3 form-control-label text-muted">{{isset($user->curatorUser) ? $user->curatorUser->tastemaker_name : ''}}</div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
     <div class="form-group row">
         <div class="col-sm-2 form-control-label">Country:</div>
         <div class="col-sm-9">
