16fd19fc2b7560c9ab7d3c279045c31d020e587e
Farhan Akram <34884414+farhanakram1@users.noreply.github.com>
2024-02-13 21:01:26 +0500

    filter done

--- resources/views/pages/curators/artist-submission/artist-submission.blade.php
+++ resources/views/pages/curators/artist-submission/artist-submission.blade.php
@@ -26,9 +26,20 @@
                     <div class="padding">
                         <div class="page-title m-b">
                             <h1 class="inline m-a-0">Artist Submission</h1>
-                            <div class="dropdown inline">
-                                <button class="btn btn-sm no-bg h4 m-y-0 v-b dropdown-toggle text-primary" data-toggle="dropdown">All</button>
-                                <div class="dropdown-menu"><a href="#" class="dropdown-item active">All</a> <a href="#" class="dropdown-item">acoustic</a> <a href="#" class="dropdown-item">ambient</a> <a href="#" class="dropdown-item">blues</a> <a href="#" class="dropdown-item">classical</a> <a href="#" class="dropdown-item">country</a> <a href="#" class="dropdown-item">electronic</a> <a href="#" class="dropdown-item">emo</a> <a href="#" class="dropdown-item">folk</a> <a href="#" class="dropdown-item">hardcore</a> <a href="#" class="dropdown-item">hip hop</a> <a href="#" class="dropdown-item">indie</a> <a href="#" class="dropdown-item">jazz</a> <a href="#" class="dropdown-item">latin</a> <a href="#" class="dropdown-item">metal</a> <a href="#" class="dropdown-item">pop</a> <a href="#" class="dropdown-item">pop punk</a> <a href="#" class="dropdown-item">punk</a> <a href="#" class="dropdown-item">reggae</a> <a href="#" class="dropdown-item">rnb</a> <a href="#" class="dropdown-item">rock</a> <a href="#" class="dropdown-item">soul</a> <a href="#" class="dropdown-item">world</a></div>
+{{--                            <div class="dropdown inline">--}}
+{{--                                <button class="btn btn-sm no-bg h4 m-y-0 v-b dropdown-toggle text-primary" data-toggle="dropdown">All</button>--}}
+{{--                                <div class="dropdown-menu"><a href="#" class="dropdown-item active">All</a> <a href="#" class="dropdown-item">acoustic</a> <a href="#" class="dropdown-item">ambient</a> <a href="#" class="dropdown-item">blues</a> <a href="#" class="dropdown-item">classical</a> <a href="#" class="dropdown-item">country</a> <a href="#" class="dropdown-item">electronic</a> <a href="#" class="dropdown-item">emo</a> <a href="#" class="dropdown-item">folk</a> <a href="#" class="dropdown-item">hardcore</a> <a href="#" class="dropdown-item">hip hop</a> <a href="#" class="dropdown-item">indie</a> <a href="#" class="dropdown-item">jazz</a> <a href="#" class="dropdown-item">latin</a> <a href="#" class="dropdown-item">metal</a> <a href="#" class="dropdown-item">pop</a> <a href="#" class="dropdown-item">pop punk</a> <a href="#" class="dropdown-item">punk</a> <a href="#" class="dropdown-item">reggae</a> <a href="#" class="dropdown-item">rnb</a> <a href="#" class="dropdown-item">rock</a> <a href="#" class="dropdown-item">soul</a> <a href="#" class="dropdown-item">world</a></div>--}}
+{{--                            </div>--}}
+                        </div>
+                        <div class="btn-group dropdown m-b m-l">
+                            <button class="btn white text-primary">Sort By</button>
+                            <button class="btn white dropdown-toggle text-primary" data-toggle="dropdown"></button>
+                            <div class="dropdown-menu pull-right">
+                                <a class="dropdown-item" href="#">Oldest</a>
+                                <a class="dropdown-item" href="#">Newest</a>
+                                <a class="dropdown-item" href="#">Release Date</a>
+                                <a class="dropdown-item" href="#">Liked Artists</a>
+                                <a class="dropdown-item" href="#">Genre</a>
                             </div>
                         </div>
                         <div data-ui-jp="jscroll" class="jscroll-loading-center" data-ui-options="{
