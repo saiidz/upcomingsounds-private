613d3af551b61b62c92be008d3c7c3375f9d5ea0
Farhan Akram <34884414+farhanakram1@users.noreply.github.com>
2024-02-16 13:54:01 +0500

    done

--- resources/views/pages/curators/curator-public-profile/public-profile.blade.php
+++ resources/views/pages/curators/curator-public-profile/public-profile.blade.php
@@ -78,6 +78,17 @@
             padding-top: 2% !important;
         }
     </style>
+    <style>
+        .x-twitter-icon::before {
+            content: "";
+            background-image: url({{asset('/images/x-twitter.png')}});
+            width: 22px;
+            height: 22px;
+            display: inline-block;
+            background-size: cover;
+            /*margin-top: 7px;*/
+        }
+    </style>
 @endsection
 
 @section('content')
