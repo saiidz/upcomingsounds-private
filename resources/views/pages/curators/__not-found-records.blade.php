a3dd7fcbd862ccff0090a94fa48ce1e2a9cca6cb
Farhan Akram <34884414+farhanakram1@users.noreply.github.com>
2024-02-21 13:32:13 +0500

    done

--- resources/views/pages/curators/__not-found-records.blade.php
+++ resources/views/pages/curators/__not-found-records.blade.php
@@ -1,3 +1,3 @@
 <div class="item-title text-ellipsis">
-    <h3 class="white" style="text-align:center; font-size:15px !important;color: black !important;">There are no result to show</h3>
+    <h3 class="white" style="text-align:center; font-size:15px !important;color: black !important;">There is nothing to show</h3>
 </div>
