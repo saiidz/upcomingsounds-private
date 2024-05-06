dcfd48f285c4e85791682eb9b84c95f6afeee786
Farhan Akram <farhanakram670@gmail.com>
2024-05-06 23:30:50 +0500

    done

--- app/Templates/I_PRODUCTS.php
+++ app/Templates/I_PRODUCTS.php
@@ -6,5 +6,6 @@ interface I_PRODUCTS
 {
 
     # 1 USC package product
-    const ONE_USC_PRODUCT = 'prod_NMz9kAFKIx11X2';
+    const ONE_USC_PRODUCT = 'prod_Q3cXuJS9Zbo7l5';
+//    const ONE_USC_PRODUCT = 'prod_NMz9kAFKIx11X2';
 }
