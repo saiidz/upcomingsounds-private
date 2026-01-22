3e5a27b369d0ab9692882cf3742bd2d99eef404e
saiidz <saiidzeidan@gmail.com>
2026-01-22 13:40:26 -0500

    Add artist-approved.blade-backup.php view file

---
+++ resources/views/pages/artists/artist-approved-admin/artist-approved.blade-backup.php
@@ -0,0 +1,67 @@
+{{-- layout --}}
+@extends('layouts.artist-guest')
+
+{{-- page title --}}
+@section('title','Application submission')
+
+{{-- page style --}}
+@section('page-style')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/themes/vertical-modern-menu-template/materialize.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/themes/vertical-modern-menu-template/style.css')}}">
+@endsection
+
+{{-- page content --}}
+@section('content')
+    <div class="app-body">
+        <div class="b-t">
+            <div class="center-block text-center">
+                <div class="p-a-md">
+                    <div>
+                        <h4><span class="Gary">Gary</span> from Upcoming Sounds</h4>
+                        <p class="text-muted m-y">
+                            Finally, you're done! You'll receive your application response shortly :D
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- BEGIN: Page Main-->
+        <div id="main">
+            <div class="row">
+                <div class="col s9 sTep2">
+                    <div class="container">
+                        <!-- Input Fields -->
+                        <div class="row">
+                            <div class="col s12">
+                                <div id="input-fields" class="card card-tabs cardsTep2">
+                                    <div class="card-content">
+                                        <div id="view-input-fields">
+                                            <div class="row">
+                                                <div class="col s12">
+                                                    <h2 class="card-title bold" style="font-size: 20px; text-align:center;"> Upcomingsounds - Artist  </h2>
+                                                    <p class="m-b-md" style="text-align:center;">Thank you for your submission request. Your application will be reviewed by our administrators. In a few days you will hear about our decision. If you haven't heard from us after 2 weeks, check your spam inbox. </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content-overlay"></div>
+                </div>
+            </div>
+        </div>
+        <!-- END: Page Main-->
+    </div>
+@endsection
+
+
+{{-- page script --}}
+@section('page-script')
+    <script src="{{asset('js/vendors.min.js')}}"></script>
+    <script src="{{asset('js/plugins.js')}}"></script>
+    <script src="{{asset('js/jquery.inputmask.bundle.min.js')}}"></script>
+    <script src="{{asset('js/intlTelInput.min.js')}}"></script>
+@endsection
