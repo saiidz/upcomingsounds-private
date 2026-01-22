ab12f168a606ab48930bafd88bf43bb144c07362
saiidz <saiidzeidan@gmail.com>
2026-01-22 11:45:19 -0500

    Update SendOfferController.php

--- app/Http/Controllers/Curator/SendOfferController.php
+++ app/Http/Controllers/Curator/SendOfferController.php
@@ -25,6 +25,15 @@ class SendOfferController extends Controller
     {
         if($request->ajax()){
             if ($request->offer_template_ID)
+           // --- START OF CUSTOM CODE ---
+            // 1. Get the Template and the Artist
+            $templateObj = CuratorOfferTemplate::find($request->offer_template_ID);
+            $artistObj = \App\Models\User::find($request->artistID);
+
+            // 2. Swap {ARTIST} with the real name
+            // (Note: If your template column is named 'message' instead of 'description', change it below)
+            $finalMessage = str_replace('{ARTIST}', $artistObj->name, $templateObj->description);
+            // --- END OF CUSTOM CODE ---
             {
                 $sendOffer = SendOffer::create([
                     'curator_id'  => Auth::id(),
@@ -35,6 +44,7 @@ class SendOfferController extends Controller
                     'expiry_date' => $request->offer_expiry,
                     'publish_date' => $request->offer_publish,
                     'is_approved' => 1,
+                    'message' => $finalMessage,                           
                 ]);
 
                 // send notification
