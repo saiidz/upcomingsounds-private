5629706f14a520f18823535e6e52fd70e554f73d
saiidz <saiidzeidan@gmail.com>
2026-01-24 13:47:56 -0500

    Update accepted.blade.php

--- resources/views/pages/artists/artist-offers/accepted.blade.php
+++ resources/views/pages/artists/artist-offers/accepted.blade.php
@@ -3,10 +3,7 @@
 {{-- page title --}}
 @section('title','Accepted')
 
-
 @section('content')
-    <!-- ############ PAGE START-->
-
     <div class="page-content">
         <div class="row-col">
             <div class="col-lg-9 b-r no-border-md">
@@ -20,67 +17,66 @@
                                 <div class="col-xs-12 remove_offer m-b" id="remove_offer-{{$sendOffer->id}}">
                                     <div class="item r Item" data-id="item-{{$sendOffer->id}}">
                                         <div class="item-media">
+                                            {{-- Profile Image Logic --}}
                                             @php
                                                 $mystring = $sendOffer->userCurator->profile;
-                                                $findhttps   = 'https';
-                                                $findhttp   = 'http';
+                                                $findhttps = 'https';
+                                                $findhttp = 'http';
                                                 $poshttps = strpos($mystring, $findhttps);
-
                                                 $poshttp = strpos($mystring, $findhttp);
-                                                if($poshttps != false){
-                                                    $pos = $poshttps;
-                                                }else{
-                                                    $pos = $poshttp;
-                                                }
+                                                $pos = ($poshttps !== false) ? $poshttps : $poshttp;
                                             @endphp
+                                            
                                             @if($pos === false)
-                                                @if(!empty($sendOffer->userCurator->profile))
-                                                    <div class="item-media-content" id="upload_profile"
-                                                         style="background-image: url({{URL('/')}}/uploads/profile/{{$sendOffer->userCurator->profile}});"></div>
-                                                @else
-                                                    <div class="item-media-content" id="upload_profile"
-                                                         style="background-image: url({{asset('images/profile_images_icons.svg')}});"></div>
-                                                @endif
-                                            @elseif($pos == 0)
                                                 <div class="item-media-content" id="upload_profile"
-                                                     style="background-image: url({{$sendOffer->userCurator->profile}});"></div>
+                                                     style="background-image: url({{ !empty($sendOffer->userCurator->profile) ? URL('/').'/uploads/profile/'.$sendOffer->userCurator->profile : asset('images/profile_images_icons.svg') }});"></div>
                                             @else
                                                 <div class="item-media-content" id="upload_profile"
-                                                     style="background-image: url({{asset('images/profile_images_icons.svg')}});"></div>
+                                                     style="background-image: url({{$sendOffer->userCurator->profile}});"></div>
                                             @endif
                                         </div>
+
                                         <div class="item-info">
                                             <div class="bottom text-right">
-                                                @if($sendOffer->status == \App\Templates\IOfferTemplateStatus::PENDING)
-                                                    <span style="color:#02b875 !important">Offer Status: </span><span class="text-danger">{{$sendOffer->status}}</span>
-                                                @else
-                                                    <span style="color:#02b875 !important">Offer Status: </span><span class="text-primary">{{$sendOffer->status}}</span>
-                                                @endif
+                                                <span style="color:#02b875 !important">Offer Status: </span>
+                                                <span class="{{ $sendOffer->status == \App\Templates\IOfferTemplateStatus::PENDING ? 'text-danger' : 'text-primary' }}">
+                                                    {{$sendOffer->status}}
+                                                </span>
                                             </div>
+                                            
                                             <div class="item-title text-ellipsis">
                                                 <span class="text-muted">{{!empty($sendOffer->userCurator) ? $sendOffer->userCurator->name : '----'}}</span>
                                             </div>
-                                            <div class="item-author text-sm text-ellipsis hide">
-                                            </div>
+
                                             <div class="item-meta text-sm text-muted">
                                                 <span class="item-meta-date text-xs">{{($sendOffer->created_at) ? \Carbon\Carbon::parse($sendOffer->created_at)->format('M d Y') : ''}}</span>
                                             </div>
 
                                             <div class="m-t-sm offerAlternative">
-                                                <div>
-                                                    <span style="color:#02b875 !important">Offer Type: </span><span class="btn btn-xs white">{{!empty($sendOffer->curatorOfferTemplate->offerType) ? $sendOffer->curatorOfferTemplate->offerType->name : '----'}}</span>
-                                                </div>
-                                                <div>
-                                                    <span style="color:#02b875 !important">Expiry Date: </span><span class="btn btn-xs white">{{($sendOffer->expiry_date) ? \Carbon\Carbon::parse($sendOffer->expiry_date)->format('M d Y') : ''}}</span>
-                                                </div>
-                                                <div>
-                                                    <span style="color:#02b875 !important">Approximate Publish Date: </span><span class="btn btn-xs white">{{($sendOffer->publish_date) ? \Carbon\Carbon::parse($sendOffer->publish_date)->format('M d Y') : ''}}</span>
-                                                </div>
+                                                <div><span style="color:#02b875 !important">Offer Type: </span><span class="btn btn-xs white">{{!empty($sendOffer->curatorOfferTemplate->offerType) ? $sendOffer->curatorOfferTemplate->offerType->name : '----'}}</span></div>
+                                                <div><span style="color:#02b875 !important">Expiry Date: </span><span class="btn btn-xs white">{{($sendOffer->expiry_date) ? \Carbon\Carbon::parse($sendOffer->expiry_date)->format('M d Y') : ''}}</span></div>
+                                                <div><span style="color:#02b875 !important">Publish Date: </span><span class="btn btn-xs white">{{($sendOffer->publish_date) ? \Carbon\Carbon::parse($sendOffer->publish_date)->format('M d Y') : ''}}</span></div>
                                             </div>
-                                            <div class="m-t-sm campaignBtn" style="display:flex">
+
+                                            <div class="m-t-sm campaignBtn" style="display:flex; gap: 10px; align-items: center;">
+                                                {{-- View Offer Button --}}
                                                 <form id="form-offer{{$sendOffer->id}}" action="{{route('artist.offer.show',encrypt($sendOffer->id))}}">
                                                     <a href="javascript:void(0)" class="btn btn-xs white" onclick="OfferShow({{$sendOffer->id}})" id="offerTemplateEdit">View Offer</a>
                                                 </form>
+
+                                                {{-- NEW: Rating Logic Integrated --}}
+                                                @php 
+                                                    $isRated = \App\Models\CuratorRating::where('offer_id', $sendOffer->id)->exists(); 
+                                                @endphp
+
+                                                @if(!$isRated)
+                                                    <button type="button" class="btn btn-xs btn-primary rounded-pill" data-toggle="modal" data-target="#rateModal{{$sendOffer->id}}">
+                                                        Rate Curator
+                                                    </button>
+                                                    @include('partials.rating_modal', ['offer' => $sendOffer])
+                                                @else
+                                                    <span class="text-success text-xs font-weight-bold"><i class="fa fa-check"></i> Feedback Rated</span>
+                                                @endif
                                             </div>
                                         </div>
                                     </div>
@@ -95,7 +91,4 @@
             @include('pages.artists.panels.right-sidebar')
         </div>
     </div>
-
-    <!-- ############ PAGE END-->
-@endsection
-
+    @endsection
