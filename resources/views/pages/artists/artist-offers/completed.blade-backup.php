5d9c4938ad2003f2227fee29d9776fa29cdb69e4
saiidz <saiidzeidan@gmail.com>
2026-01-25 12:55:44 -0500

    Create completed.blade-backup.php

---
+++ resources/views/pages/artists/artist-offers/completed.blade-backup.php
@@ -0,0 +1,152 @@
+
+@extends('pages.artists.panels.layout')
+
+{{-- page title --}}
+@section('title','Completed')
+
+
+@section('content')
+    <div class="page-content">
+        <div class="row-col">
+            <div class="col-lg-9 b-r no-border-md">
+                <div class="padding">
+                    <div class="page-title m-b">
+                        <h1 class="inline m-a-0 titleColor">Completed</h1>
+                    </div>
+                    <div class="row item-list item-list-by m-b">
+                        @if(!empty($sendOffers) && count($sendOffers) > 0)
+                            @foreach($sendOffers as $sendOffer)
+                                <div class="col-xs-12 remove_offer m-b" id="remove_offer-{{$sendOffer->id}}">
+                                    <div class="item r Item" data-id="item-{{$sendOffer->id}}">
+                                        <div class="item-media">
+                                            @php
+                                                $mystring = $sendOffer->userCurator->profile;
+                                                $findhttps   = 'https';
+                                                $findhttp   = 'http';
+                                                $poshttps = strpos($mystring, $findhttps);
+
+                                                $poshttp = strpos($mystring, $findhttp);
+                                                if($poshttps !== false){
+                                                    $pos = $poshttps;
+                                                }else{
+                                                    $pos = $poshttp;
+                                                }
+                                            @endphp
+                                            @if($pos === false)
+                                                @if(!empty($sendOffer->userCurator->profile))
+                                                    <div class="item-media-content" id="upload_profile"
+                                                         style="background-image: url({{URL('/')}}/uploads/profile/{{$sendOffer->userCurator->profile}});"></div>
+                                                @else
+                                                    <div class="item-media-content" id="upload_profile"
+                                                         style="background-image: url({{asset('images/profile_images_icons.svg')}});"></div>
+                                                @endif
+                                            @elseif($pos == 0)
+                                                <div class="item-media-content" id="upload_profile"
+                                                     style="background-image: url({{$sendOffer->userCurator->profile}});"></div>
+                                            @else
+                                                <div class="item-media-content" id="upload_profile"
+                                                     style="background-image: url({{asset('images/profile_images_icons.svg')}});"></div>
+                                            @endif
+                                        </div>
+                                        <div class="item-info">
+                                            <div class="bottom text-right">
+                                                <span style="color:#02b875 !important">Offer Status: </span><span class="text-primary">{{$sendOffer->status}}</span>
+                                            </div>
+                                            <div class="item-title text-ellipsis">
+                                                <span class="text-muted">{{!empty($sendOffer->userCurator) ? $sendOffer->userCurator->name : '----'}}</span>
+                                            </div>
+                                            <div class="item-author text-sm text-ellipsis hide">
+                                            </div>
+                                            <div class="item-meta text-sm text-muted">
+                                                <span class="item-meta-date text-xs">{{($sendOffer->created_at) ? \Carbon\Carbon::parse($sendOffer->created_at)->format('M d Y') : ''}}</span>
+                                            </div>
+
+                                            <div class="m-t-sm offerAlternative">
+                                                <div>
+                                                    <span style="color:#02b875 !important">Offer Type: </span><span class="btn btn-xs white">{{!empty($sendOffer->curatorOfferTemplate->offerType) ? $sendOffer->curatorOfferTemplate->offerType->name : '----'}}</span>
+                                                </div>
+                                                <div>
+                                                    <span style="color:#02b875 !important">Expiry Date: </span><span class="btn btn-xs white">{{($sendOffer->expiry_date) ? \Carbon\Carbon::parse($sendOffer->expiry_date)->format('M d Y') : ''}}</span>
+                                                </div>
+                                                <div>
+                                                    <span style="color:#02b875 !important">Approximate Publish Date: </span><span class="btn btn-xs white">{{($sendOffer->publish_date) ? \Carbon\Carbon::parse($sendOffer->publish_date)->format('M d Y') : ''}}</span>
+                                                </div>
+                                            </div>
+
+                                            {{-- UPDATED CAMPAIGN BUTTON SECTION --}}
+                                            <div class="m-t-sm campaignBtn" id="cOfferBtn" style="display: flex; gap: 8px; align-items: center; flex-wrap: wrap;">
+                                                <div>
+                                                    <form id="form-offer{{$sendOffer->id}}" action="{{route('artist.offer.show',encrypt($sendOffer->id))}}">
+                                                        <a href="javascript:void(0)" class="btn btn-xs white" onclick="OfferShow({{$sendOffer->id}})" id="offerTemplateEdit">View Offer</a>
+                                                    </form>
+                                                </div>
+
+                                                {{-- Check if the artist has already rated this completed curator --}}
+                                                @php 
+                                                    $isRated = \App\Models\CuratorRating::where('offer_id', $sendOffer->id)->exists(); 
+                                                @endphp
+
+                                                @if(!$isRated)
+                                                    <button type="button" class="btn btn-xs btn-primary rounded-pill" data-toggle="modal" data-target="#rateModal{{$sendOffer->id}}">
+                                                        Rate Experience
+                                                    </button>
+                                                    @include('partials.rating_modal', ['offer' => $sendOffer])
+                                                @else
+                                                    <span class="text-success text-xs font-weight-bold" style="padding-left: 5px;">
+                                                        <i class="fa fa-check-circle"></i> Feedback Rated
+                                                    </span>
+                                                @endif
+
+                                                @if(!empty($sendOffer) && $sendOffer->status == \App\Templates\IOfferTemplateStatus::COMPLETED
+                                                && !empty($sendOffer->sendOfferTransaction) && $sendOffer->sendOfferTransaction->status == \App\Templates\IOfferTemplateStatus::REFUND)
+                                                    <div>
+                                                        <span id="mgAdmin{{$sendOffer->id}}" style="display:none">{!! $sendOffer->sendOfferTransaction->refund_message !!}</span>
+                                                        <a href="javascript:void(0)" class="btn btn-xs white"  onclick="completedOfferMsgModal({{$sendOffer->id}})">
+                                                            Refund Offer Message
+                                                        </a>
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            @include('pages.curators.__not-found-records')
+                        @endif
+                    </div>
+                </div>
+            </div>
+            @include('pages.artists.panels.right-sidebar')
+        </div>
+    </div>
+
+    <div id="completedMsgModalCenter" class="modal fade black-overlay" data-backdrop="false">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Refund Message Completed Offer</h5>
+                </div>
+                <div class="modal-body">
+                    <p id="msgCompletedCurator"></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div></div>
+    </div>
+@endsection
+
+@section('page-script')
+    <script>
+        function OfferShow(id) {
+            $("#form-offer" + id).submit();
+        }
+
+        function completedOfferMsgModal(id) {
+            let msg = $('#mgAdmin'+id).html();
+            $('#msgCompletedCurator').html(msg);
+            $('#completedMsgModalCenter').modal('show');
+        }
+    </script>
+@endsection
