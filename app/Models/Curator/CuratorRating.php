dbca23058e41c5a8cd48dfdbdcd042d43b81cc1a
saiidz <saiidzeidan@gmail.com>
2026-01-25 16:19:13 -0500

    Update CuratorRating.php

--- app/Models/Curator/CuratorRating.php
+++ app/Models/Curator/CuratorRating.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models; // Add the semicolon here
+namespace App\Models\Curator;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
