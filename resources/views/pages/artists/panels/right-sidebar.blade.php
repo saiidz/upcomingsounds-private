5d645f375189bae83ee9ec148ef84b1207059b88
saiidz <saiidzeidan@gmail.com>
2026-01-24 19:19:56 -0500

    Update right-sidebar.blade.php

--- resources/views/pages/artists/panels/right-sidebar.blade.php
+++ resources/views/pages/artists/panels/right-sidebar.blade.php
@@ -1,133 +1,18 @@
 <div class="col-lg-3 w-xxl w-auto-md">
     <div class="padding" style="bottom: 60px;" data-ui-jp="stick_in_parent">
-{{--        <h6 class="text text-muted">5 Likes</h6>--}}
-{{--        <div class="row item-list item-list-sm m-b">--}}
-{{--            <div class="col-xs-12">--}}
-{{--                <div class="item r" data-id="item-3"--}}
-{{--                     data-src="http://api.soundcloud.com/tracks/79031167/stream?client_id=a10d44d431ad52868f1bce6d36f5234c">--}}
-{{--                    <div class="item-media ">--}}
-{{--                        <a href="javascript:void(0)" class="item-media-content"--}}
-{{--                           style="background-image: url('images/b2.jpg');"></a>--}}
-{{--                    </div>--}}
-{{--                    <div class="item-info">--}}
-{{--                        <div class="item-title text-ellipsis">--}}
-{{--                            <a href="javascript:void(0)">I Wanna Be In the Cavalry</a>--}}
-{{--                        </div>--}}
-{{--                        <div class="item-author text-sm text-ellipsis ">--}}
-{{--                            <a href="javascript:void(0)" class="text-muted">Jeremy Scott</a>--}}
-{{--                        </div>--}}
-
-
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-xs-12">--}}
-{{--                <div class="item r" data-id="item-1"--}}
-{{--                     data-src="http://api.soundcloud.com/tracks/269944843/stream?client_id=a10d44d431ad52868f1bce6d36f5234c">--}}
-{{--                    <div class="item-media ">--}}
-{{--                        <a href="javascript:void(0)" class="item-media-content"--}}
-{{--                           style="background-image: url('images/b0.jpg');"></a>--}}
-{{--                    </div>--}}
-{{--                    <div class="item-info">--}}
-{{--                        <div class="item-title text-ellipsis">--}}
-{{--                            <a href="javascript:void(0)">Pull Up</a>--}}
-{{--                        </div>--}}
-{{--                        <div class="item-author text-sm text-ellipsis ">--}}
-{{--                            <a href="javascript:void(0)" class="text-muted">Summerella</a>--}}
-{{--                        </div>--}}
-
-
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-xs-12">--}}
-{{--                <div class="item r" data-id="item-12"--}}
-{{--                     data-src="http://api.soundcloud.com/tracks/174495152/stream?client_id=a10d44d431ad52868f1bce6d36f5234c">--}}
-{{--                    <div class="item-media ">--}}
-{{--                        <a href="javascript:void(0)" class="item-media-content"--}}
-{{--                           style="background-image: url('images/b11.jpg');"></a>--}}
-{{--                    </div>--}}
-{{--                    <div class="item-info">--}}
-{{--                        <div class="item-title text-ellipsis">--}}
-{{--                            <a href="javascript:void(0)">Happy ending</a>--}}
-{{--                        </div>--}}
-{{--                        <div class="item-author text-sm text-ellipsis ">--}}
-{{--                            <a href="javascript:void(0)" class="text-muted">Postiljonen</a>--}}
-{{--                        </div>--}}
-
-
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-xs-12">--}}
-{{--                <div class="item r" data-id="item-11"--}}
-{{--                     data-src="http://api.soundcloud.com/tracks/218060449/stream?client_id=a10d44d431ad52868f1bce6d36f5234c">--}}
-{{--                    <div class="item-media ">--}}
-{{--                        <a href="javascript:void(0)" class="item-media-content"--}}
-{{--                           style="background-image: url('images/b10.jpg');"></a>--}}
-{{--                    </div>--}}
-{{--                    <div class="item-info">--}}
-{{--                        <div class="item-title text-ellipsis">--}}
-{{--                            <a href="javascript:void(0)">Spring</a>--}}
-{{--                        </div>--}}
-{{--                        <div class="item-author text-sm text-ellipsis ">--}}
-{{--                            <a href="javascript:void(0)" class="text-muted">Pablo Nouvelle</a>--}}
-{{--                        </div>--}}
-
-
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-xs-12">--}}
-{{--                <div class="item r" data-id="item-6"--}}
-{{--                     data-src="http://api.soundcloud.com/tracks/236107824/stream?client_id=a10d44d431ad52868f1bce6d36f5234c">--}}
-{{--                    <div class="item-media ">--}}
-{{--                        <a href="javascript:void(0)" class="item-media-content"--}}
-{{--                           style="background-image: url('images/b5.jpg');"></a>--}}
-{{--                    </div>--}}
-{{--                    <div class="item-info">--}}
-{{--                        <div class="item-title text-ellipsis">--}}
-{{--                            <a href="javascript:void(0)">Body on me</a>--}}
-{{--                        </div>--}}
-{{--                        <div class="item-author text-sm text-ellipsis ">--}}
-{{--                            <a href="javascript:void(0)" class="text-muted">Rita Ora</a>--}}
-{{--                        </div>--}}
-
-
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <h6 class="text text-muted">Go mobile</h6>--}}
-{{--        <div class="btn-groups">--}}
-{{--            <a href="" class="btn btn-sm dark lt m-r-xs" style="width: 135px">--}}
-{{--<span class="pull-left m-r-sm">--}}
-{{--<i class="fa fa-apple fa-2x"></i>--}}
-{{--</span>--}}
-{{--                <span class="clear text-left l-h-1x">--}}
-{{--<span class="text-muted text-xxs">Download on the</span>--}}
-{{--<b class="block m-b-xs">App Store</b>--}}
-{{--</span>--}}
-{{--            </a>--}}
-{{--            <a href="" class="btn btn-sm dark lt" style="width: 133px">--}}
-{{--<span class="pull-left m-r-sm">--}}
-{{--<i class="fa fa-play fa-2x"></i>--}}
-{{--</span>--}}
-{{--                <span class="clear text-left l-h-1x">--}}
-{{--<span class="text-muted text-xxs">Get it on</span>--}}
-{{--<b class="block m-b-xs m-r-xs">Google Play</b>--}}
-{{--</span>--}}
-{{--            </a>--}}
-{{--        </div>--}}
         <div class="b-b m-y"></div>
+        
+        {{-- Profile Info (Bulletproof fallback) --}}
+        <div class="nav text-sm _600">
+            <p class="text-muted text-xs">Logged in as: <strong>{{ auth()->user()->name ?? 'Artist' }}</strong></p>
+        </div>
+
         <div class="nav text-sm _600">
             <a href="{{ url('about-us') }}" class="nav-link text-muted m-r-xs">About</a>
             <a href="{{ url('contact-us') }}" class="nav-link text-muted m-r-xs">Contact</a>
             <a href="{{ url('term-of-service') }}" class="nav-link text-muted m-r-xs">Term of Service</a>
-            {{-- <a href="#" class="nav-link text-muted m-r-xs">Legal</a> --}}
             <a href="{{ url('privacy-policy') }}" class="nav-link text-muted m-r-xs">Policy Privacy</a>
-
         </div>
-        <p class="text-muted text-xs p-b-lg">&copy; Copyright {{ date('Y') }}</p>
+        <p class="text-muted text-xs p-b-lg">&copy; Copyright {{ date('Y') }} UpComing Sounds</p>
     </div>
 </div>
