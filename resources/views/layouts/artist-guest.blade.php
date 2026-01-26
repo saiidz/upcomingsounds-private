95b66a65730d840375599fe752221b898a5d8e13
saiidz <saiidzeidan@gmail.com>
2026-01-26 16:11:57 -0500

    Update artist-guest.blade.php

--- resources/views/layouts/artist-guest.blade.php
+++ resources/views/layouts/artist-guest.blade.php
@@ -1,93 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- BEGIN: Head-->
-{{-- Include core + vendor Styles --}}
-@include('panels.auth-styles')
-<!-- END: Head-->
+<head>
+    {{-- Safety check for style include --}}
+    @if(view()->exists('panels.auth-styles'))
+        @include('panels.auth-styles')
+    @else
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @endif
+</head>
 
 <body>
 <div class="app dk" id="app">
     <div id="snackbar"></div>
     <div id="snackbarError"></div>
-    <!-- content -->
+    
     <div id="content" class="app-content" role="main">
-
         <div class="app-header navbar-md black box-shadow-z1">
             <div class="navbar" data-pjax>
-                {{-- <a data-toggle="collapse" data-target="#navbar"
-                   class="navbar-item pull-right hidden-md-up m-r-0 m-l">
-                    <i class="material-icons">menu</i>
-                </a> --}}
-                <!-- brand -->
-                <a href="{{url('/')}}" class="navbar-brand md">
-                    <img src="{{asset('images/logo.png')}}" alt="">
+                <a href="{{ url('/') }}" class="navbar-brand md">
+                    {{-- Uses the theme variable from our AppServiceProvider fix --}}
+                    <img src="{{ asset($theme->logo ?? 'images/logo.png') }}" alt="Logo">
                 </a>
-                <!-- / brand -->
 
-                <!-- nabar right -->
                 <ul class="nav navbar-nav pull-right">
                     @if(Auth::check())
                         <li class="nav-item">
-                            <a href="{{ route('logout') }}" class="nav-link">
-                                Sign out
-                            </a>
+                            {{-- Standard Laravel Logout logic (Prevents 405/500 errors) --}}
+                            @if(Route::has('logout'))
+                                <a href="{{ route('logout') }}" class="nav-link" 
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    Sign out
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            @endif
                         </li>
                     @else
-                        <li class="nav-item">
-                            <a href="{{ route('register') }}" class="nav-link">
-                                Signup
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('login') }}" class="nav-link">
-                    <span class="btn btn-sm rounded primary _600">
-                      Signin
-                    </span>
-                            </a>
-                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a href="{{ route('register') }}" class="nav-link">Signup</a>
+                            </li>
+                        @endif
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a href="{{ route('login') }}" class="nav-link">
+                                    <span class="btn btn-sm rounded primary _600">Signin</span>
+                                </a>
+                            </li>
+                        @endif
                     @endif
-
                 </ul>
-                <!-- / navbar right -->
 
-                <!-- navbar collapse -->
                 <div class="collapse navbar-toggleable-sm l-h-0 text-center" id="navbar">
-                    <!-- link and dropdown -->
                     <ul class="nav navbar-nav nav-md inline text-primary-hover">
-{{--                        <li class="nav-item">--}}
-{{--                            <a href="#" class="nav-link">--}}
-{{--                                <span class="nav-text">Site</span>--}}
-{{--                            </a>--}}
-{{--                        </li>--}}
-                        {{-- <li class="nav-item">
-                            <a href="{{url('/artist-profile')}}" class="nav-link">
+                        <li class="nav-item">
+                            <a href="{{ url('/artist-offers') }}" class="nav-link">
                                 <span class="nav-text">Dashboard</span>
                             </a>
-                        </li> --}}
-{{--                        <li class="nav-item">--}}
-{{--                            <a href="#" class="nav-link">--}}
-{{--                                <span class="nav-text">Rtl</span>--}}
-{{--                            </a>--}}
-{{--                        </li>--}}
+                        </li>
                     </ul>
-                    <!-- / link and dropdown -->
                 </div>
-                <!-- / navbar collapse -->
             </div>
         </div>
 
         @yield('content')
-        @include('panels.footer')
-        {{-- @include('welcome-panels.welcome-footer') --}}
+        
+        @if(view()->exists('panels.footer'))
+            @include('panels.footer')
+        @endif
     </div>
-    <!-- / -->
-
-
-{{--@yield('content')--}}
-
-<!-- BEGIN VENDOR JS-->
-    <!-- END PAGE LEVEL JS-->
 </div>
-@include('panels.auth-scripts')
+
+@if(view()->exists('panels.auth-scripts'))
+    @include('panels.auth-scripts')
+@endif
 </body>
 </html>
