782a669a408fbcbe3e6dfff17d49d11bb5b5c758
saiidz <saiidzeidan@gmail.com>
2026-02-02 15:41:39 -0500

    Update OfferController.php

--- app/Http/Controllers/Artist/OfferController.php
+++ app/Http/Controllers/Artist/OfferController.php
@@ -1,15 +1,24 @@
 <?php
+
 namespace App\Http\Controllers\Artist;
+
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\ArtistTrack;
 use App\Models\Genre;
-class OfferController extends Controller {
-    public function offers() {
+use App\Models\SendOffer;
+use App\Models\Curator\CuratorRating;
+use App\Models\Conversation;
+use Illuminate\Http\Request;
+
+class OfferController extends Controller 
+{
+    public function offers() 
+    {
         $user = auth()->user();
         $artist_tracks = ArtistTrack::where('user_id', $user->id)->get();
         $genres = Genre::all();
-        $box = []; $flag = ""; $pos = 0; // The 2024 design variables
+        $box = []; $flag = ""; $pos = 0; 
         return view('pages.artists.artist-track.index', get_defined_vars());
     }
 }
