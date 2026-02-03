13a24329d434ddafe8f0728deddc41bca68348e0
saiidz <saiidzeidan@gmail.com>
2026-02-03 11:33:43 -0500

    Update offers.blade.php

--- resources/views/pages/artists/artist-offers/offers.blade.php
+++ resources/views/pages/artists/artist-offers/offers.blade.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 @extends('pages.artists.panels.layout')
 
 {{-- page title --}}
