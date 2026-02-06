2ba7b04cc1aa004f5edb54332d3235bba762a98d
saiidz <saiidzeidan@gmail.com>
2026-02-06 16:05:42 -0500

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,9 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\LaravelCors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -33,13 +34,15 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\StoreReferralCode::class,
+            \App\Http\Middleware\TrackLastActiveAt::class,
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -61,6 +64,10 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'create_password' => \App\Http\Middleware\CheckCreatePassword::class,
+        'create_curator_password' => \App\Http\Middleware\CheckCuratorCreatePassword::class,
+        'artist_signup' => \App\Http\Middleware\EnsureSignUpArtistIsCompleted::class,
+        'curator_signup' => \App\Http\Middleware\EnsureSignUpCuratorIsCompleted::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'verified_phone_number_curator' => \App\Http\Middleware\VerifyPhoneNumberCurator::class,
         'try_catch' => \App\Http\Middleware\TryCatchMiddleware::class,
